0df45a77fbc0ba497fa1661614ffd7966dfbe7b9
Sanju Shree Varshini <sanjushreevarshini.kk2023ece@sece.ac.in>
2025-01-21 12:08:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Menu from './Menu'; // Import Menu component
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Menu /> {/* Render the Menu component */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Menu.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const Menu = () => {
+  const [count, newcount] = useState(0);
+
+  const add = () => newcount(count + 1);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={add}>Counter</button>
+    </div>
+  );
+};
+
+export default Menu;
